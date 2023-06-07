94b10d51528ce8e674adc19cc8739c8dd8e24000
Ayush P Vachhani <ayupatel107@gmail.com>
2023-06-07 10:20:44 +0530

    without json

--- api/views.py
+++ api/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def first_api(request):
-    data = {"message": "This message has been routed from age","age":100}
+    data = {"message": "This message has been routed from api"}
     return JsonResponse(data)
